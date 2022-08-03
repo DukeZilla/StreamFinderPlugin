4397bf0366ea4b383ae6f49935473e5bff271888
DukeZilla <59232365+DukeZilla@users.noreply.github.com>
2022-08-03 16:55:54 -0400

    Add files via upload

---
+++ StreamFinderPlugin.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#include "bakkesmod/plugin/pluginwindow.h"
+#include "bakkesmod/plugin/PluginSettingsWindow.h"
+
+#include "version.h"
+
+#pragma comment( lib, "pluginsdk.lib" )
+
+constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
+
+
+class StreamFinderPlugin: public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow, public BakkesMod::Plugin::PluginWindow
+{
+
+	//std::shared_ptr<bool> enabled;
+
+	// Inherited via PluginSettingsWindow
+	/*
+	void RenderSettings() override;
+	std::string GetPluginName() override;
+	void SetImGuiContext(uintptr_t ctx) override;
+	*/
+
+	// Inherited via PluginWindow
+	/*
+
+	bool isWindowOpen_ = false;
+	bool isMinimized_ = false;
+	std::string menuTitle_ = "StreamFinderPlugin";
+
+	virtual void Render() override;
+	virtual std::string GetMenuName() override;
+	virtual std::string GetMenuTitle() override;
+	virtual void SetImGuiContext(uintptr_t ctx) override;
+	virtual bool ShouldBlockInput() override;
+	virtual bool IsActiveOverlay() override;
+	virtual void OnOpen() override;
+	virtual void OnClose() override;
+	
+	*/
+public:
+
+	StreamFinderPlugin() : gameWrapper(GameWrapper(NULL)) {}
+
+	virtual void onLoad() override;
+	virtual void onUnload() override;
+
+	void LoadHooks();
+	void RenderDrawable(CanvasWrapper canvas);
+	void OpenScoreboard(std::string eventName);
+	void CloseScoreboard(std::string eventName);
+	void LaunchQuickNoteModal();
+
+#pragma region GUI
+
+	void Render() override;
+	void RenderEditNoteModal();
+	std::string GetMenuName() override;
+	std::string GetMenuTitle() override;
+	void SetImGuiContext(uintptr_t ctx) override;
+	bool ShouldBlockInput() override;
+	bool IsActiveOverlay() override;
+	void OnOpen() override;
+	void OnClose() override;
+	void OpenMenu();
+	void CloseMenu();
+	void ToggleMenu();
+
+private:
+	void Log(std::string msg);
+	bool isWindowOpen = false;
+	bool shouldBlockInput = false;
+	std::string menuTitle = "Stream Sniper";
+	bool openQuickNote = false;
+	std::string playersNoteToEdit = "";
+};
+
