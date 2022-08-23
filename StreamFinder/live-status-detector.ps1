b347cbd36a2180e95f62cc73d8bcf7ac48a2a967
DukeZilla <59232365+DukeZilla@users.noreply.github.com>
2022-08-22 20:00:00 -0400

    Update live-status-detector.ps1

--- StreamFinder/live-status-detector.ps1
+++ StreamFinder/live-status-detector.ps1
@@ -46,18 +46,28 @@ return $authHeadTwitch
 
 iwr -method post -uri $twitchUri -UseBasicParsing
 $p = (pwd).path
-$i = 0
+$i = -1
 
 Get-Content discord-webhook.txt | Foreach-Object{
 	$discord_webhook = $_.Split('')
 	New-Variable -Name $discord_webhook[0] -Value $discord_webhook[1]
 }
 
-echo "Your name has been skipped"
+if ($discord_webhook -eq $null) {
+Add-Type -AssemblyName System.Windows.Forms
+$global:balmsg = New-Object System.Windows.Forms.NotifyIcon
+$path = (Get-Process -id $pid).Path
+$balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+$balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
+$balmsg.BalloonTipText = 'Discord webhook not found, in order to get notifications of live streamers, include a webhook link in the discord-webhook.txt file.'
+$balmsg.BalloonTipTitle = "Stream Finder Plugin Error"
+$balmsg.Visible = $true
+$balmsg.ShowBalloonTip(60000)
+}
+
+echo "Stream search beginning."
 function nameloop {
 	$i++
-	echo "----------------------------------------------------------0"
-	echo "Instance #$i"
 	
 	# Name search refinement
 	$names = gc $p\names.txt
@@ -66,12 +76,17 @@ function nameloop {
 	$perma_blacklist = gc $p\permanent-blacklist.txt -erroraction silentlycontinue
 	$name = $names | select -index $i
 	
+	echo "----------------------------------------------------------0"
+	echo "Instance #$i"
+	
 	# Null Value Check / Breaker
 	$null_check = $name
     if ($null -eq $null_check) { # at the end of the list the loop will break
-		echo 'null value break'
+		echo "Search halted, reason: null value break"
+		echo "Reached the end of name list."
 		$names > blacklist.txt
 		echo "----------------------------------------------------------0"
+		echo "Stream search ended."
 		break
 	}
 	
@@ -91,26 +106,29 @@ function nameloop {
 	#	$name = $name -split "\stwitch\s"
 	#	echo 'live name split'
 	#}
-	echo "Tested name = $name"
+	echo "Tested name = ""$name"""
 	# Breakers
 	if ($i -eq  10) { # to prevent from looping more than necessary
-		echo 'max number break'
+		echo 'Search halted, reason: Max number break'
 		$names > blacklist.txt
 		echo "----------------------------------------------------------0"
+		echo "Stream search ended."
 		break
 	}
 	if ($perma_blacklist -contains $old_name) { # to prevent from sending a notification of the same live streamer for one game
 		echo "Player name $old_name is permanetaly blacklisted"
+		echo "Search skipped."
 		nameloop
 	}
 	if ($blacklist -contains $old_name) { # to prevent from sending a notification of the same live streamer for one game
 		echo "Player name $old_name has been temporarily blacklisted"
+		echo "Search skipped."
 		nameloop
 	}
 	
 	streamsearch
 	echo "Searched $old_name"
-	echo "=-=-=-=-=-=-=-=-=-=-=-=-="
+	echo "=-=-=-=-=-=-=-=-=-=-=-=-=-="
 	$name = $name.trim('')
 	$name = $name.trim(' ')
 	$name = $name | where{$_ -ne ""}
@@ -130,11 +148,12 @@ function nameloop {
 			$rcount = $x+1
 			echo "Split search instance #$rcount"
 			$name = $split_name -split "_" | select -index $x
-			echo "Researched $name"
+			write-host "Researching $name" -foregroundcolor yellow
 			streamsearch
 		}
 	}
-	echo "=-=-=-=-=-=-=-=-=-=-=-=-="
+	write-host "No live streamers detected." -foregroundcolor red
+	echo "=-=-=-=-=-=-=-=-=-=-=-=-=-="
 	echo "Search on $old_name has been terminated."
 	nameloop
 }
@@ -143,7 +162,7 @@ function streamsearch {
 	$requestRAW = Invoke-WebRequest -Headers (Get-AuthenticationHeaderTwitch) -UseBasicParsing -Uri https://api.twitch.tv/helix/search/channels?query=$name
 	$live_status = (ConvertFrom-Json ($requestRAW)).Data -match "Rocket League" | select -property broadcaster_login, is_live, game_name | where{$_.is_live -match "True"}
 	if ($live_status -like "*True*") { # Discord Bot send notifications via webhook to server
-		echo "Live streamer found! => $old_name"
+		write-host "Live streamer found! => $old_name" -foregroundcolor green
 		$split00 = $live_status | select -expandproperty broadcaster_login # Isolating the broadcaster's name
 		$trim00 = $split00 | out-string
 		$twitch_username = $trim00.trim('')
@@ -154,10 +173,13 @@ function streamsearch {
 		content = $content
 		}
 		iwr -uri $url -method Post -body ($payload | ConvertTo-Json) -ContentType 'Application/Json'
+		echo "Discord notification sent!"
+		echo "Discord url = $url"
 		$trim01 = get-date | out-string # Log file
 		$date = $trim01.trim('')
 		echo "Streamer $twitch_username was found live on $date" >> livestreamlog.txt
 		echo "Searched $old_name"
+		echo "Search on $old_name has been terminated."
 		nameloop
 	}
 }
