162cd6055d12dd023feb89ce9ba1ec82d7cc170d
DukeZilla <59232365+DukeZilla@users.noreply.github.com>
2022-10-08 14:48:54 -0400

    Update StreamFinderPlugin.h

--- StreamFinderPlugin.h
+++ StreamFinderPlugin.h
@@ -3,6 +3,9 @@
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
+#include "bakkesmod/wrappers/GuiManagerWrapper.h"
+#include <sstream>
+#include <fstream>
 
 #include "version.h"
 
@@ -11,56 +14,57 @@
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
 
-class StreamFinderPlugin : public BakkesMod::Plugin::BakkesModPlugin 
+class StreamFinderPlugin : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
 {
 
-	//std::shared_ptr<bool> enabled;
+	// std::shared_ptr<bool> enabled;
 
 	// Inherited via PluginSettingsWindow
-	/*
+	
 	void RenderSettings() override;
 	std::string GetPluginName() override;
 	void SetImGuiContext(uintptr_t ctx) override;
-	*/
+	
 
 	// Inherited via PluginWindow
-	/*
+	
 
 	bool isWindowOpen_ = false;
 	bool isMinimized_ = false;
-	std::string menuTitle_ = "StreamFinderPlugin";
-
-	virtual void Render() override;
-	virtual std::string GetMenuName() override;
-	virtual std::string GetMenuTitle() override;
-	virtual void SetImGuiContext(uintptr_t ctx) override;
-	virtual bool ShouldBlockInput() override;
-	virtual bool IsActiveOverlay() override;
-	virtual void OnOpen() override;
-	virtual void OnClose() override;
+	std::string menuTitle_ = "Stream Finder Plugin";	
 	
-	*/
 public:
 
 	virtual void onLoad() override;
 	virtual void onUnload() override;
 
 	void LoadHooks();
+	/*
 	void RenderDrawable(CanvasWrapper canvas);
 	void OpenScoreboard(std::string eventName);
 	void CloseScoreboard(std::string eventName);
+	void HandlePlayerAdded(std::string eventName);
+	*/
+
 	void LaunchQuickNoteModal();
 	void HandleGameStart(std::string eventName);
-	void HandleGameEnd(std::string eventName);
-	void HandleGameLeave(std::string eventName);
-	void HandlePlayerAdded(std::string eventName);
 
 #pragma region GUI
 
+	
+	virtual void Render() override;
+	std::string GetMenuName() override;
+	std::string GetMenuTitle() override;
+	virtual bool ShouldBlockInput() override;
+	virtual bool IsActiveOverlay() override;
+	virtual void OnOpen() override;
+	virtual void OnClose() override;
+	virtual void SetImGuiContext(uintptr_t ctx) override;
 	void RenderEditNoteModal();
 	void OpenMenu();
 	void CloseMenu();
-	void ToggleMenu();
+	void ToggleMenu(); 
+	
 	std::vector<std::string> GetPlayersNames();
 
 private:
@@ -70,5 +74,6 @@ private:
 	std::string menuTitle = "Stream Finder Plugin";
 	bool openQuickNote = false;
 	std::string playersNoteToEdit = "";
+#pragma endregion GUI
 };
 
