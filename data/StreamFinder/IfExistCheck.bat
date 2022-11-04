d4e8c3fb467a724f0f52b1126805de4fa674b943
Daniel Rodriguez <59232365+DukeZilla@users.noreply.github.com>
2022-11-03 22:25:51 -0400

    Update IfExistCheck.bat

--- data/StreamFinder/IfExistCheck.bat
+++ data/StreamFinder/IfExistCheck.bat
@@ -8,4 +8,5 @@ goto main
 IF EXIST PeaceOfMind.txt (echo "Pom Exists") else (echo " " >> PeaceOfMind.txt)
 IF EXIST Session-Blacklist.txt (echo "Ses Exists") else (echo " " >> Session-Blacklist.txt)
 IF EXIST livestreamlog.txt (echo "Log Exists") else (echo " " >> livestreamlog.txt)
+taskkill /im cmd.exe -f
 exit
