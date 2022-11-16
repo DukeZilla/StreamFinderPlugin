c75463307ae4082bbf9e3cb62703b6005baae58b
Daniel Rodriguez <59232365+DukeZilla@users.noreply.github.com>
2022-11-16 04:22:38 -0500

    Update Streamlink-Recorder.bat

--- data/StreamFinder/Streamlink-Recorder.bat
+++ data/StreamFinder/Streamlink-Recorder.bat
@@ -4,18 +4,14 @@ title Stream Finder Plugin ^| Streamlink Recorder
 PUSHD "%CD%"
 CD /D "%~dp0"
 set back=%cd%
-cd %appdata%
-cd ..
-cd local
-set local=%cd%
-cd %back%
 cls
 goto main
 
 :main
 set DateTime=%DATE:~-4%.%DATE:~4,2%.%DATE:~7,2%%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%
+for /f "usebackq tokens=1,* delims=" %%x in (Path.txt) do (set streamlink=%%x)
 for /f "usebackq tokens=1,* delims=" %%x in (streamlink-info.txt) do (set %%x)
 IF EXIST Recordings (cd Recordings) else (mkdir Recordings & cd Recordings)
-call %local%\Streamlink\bin\streamlink -o "%TwitchUsr% (%DateTime%).ts" "%TwitchLink%" 1080p,best
+call "%streamlink%" -o "%TwitchUsr% (%DateTime%).ts" "%TwitchLink%" 1080p,best
 IF %ERRORLEVEL% == 1 (cd %back% & echo Unable to record, %TwitchUsr% is not live. > rec-status.txt)
 exit
