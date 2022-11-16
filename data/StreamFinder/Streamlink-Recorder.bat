290d7abed28d17baab53dfaeccccfbaece3d717c
Daniel Rodriguez <59232365+DukeZilla@users.noreply.github.com>
2022-11-16 17:06:16 -0500

    Update Streamlink-Recorder.bat

--- data/StreamFinder/Streamlink-Recorder.bat
+++ data/StreamFinder/Streamlink-Recorder.bat
@@ -9,7 +9,7 @@ goto main
 
 :main
 set DateTime=%DATE:~-4%.%DATE:~4,2%.%DATE:~7,2%%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%
-for /f "usebackq tokens=1,* delims=" %%x in (Path.txt) do (set streamlink=%%x)
+for /f "usebackq tokens=1,* delims=" %%x in (Path-streamlink.txt) do (set streamlink=%%x)
 for /f "usebackq tokens=1,* delims=" %%x in (streamlink-info.txt) do (set %%x)
 IF EXIST Recordings (cd Recordings) else (mkdir Recordings & cd Recordings)
 call "%streamlink%" -o "%TwitchUsr% (%DateTime%).ts" "%TwitchLink%" 1080p,best
