258649f2eba8680d4b19eacc62bb108ade2eccaa
Daniel Rodriguez <59232365+DukeZilla@users.noreply.github.com>
2022-11-22 20:58:19 -0500

    Add files via upload

---
+++ data/StreamFinder/Check-Update.bat
@@ -0,0 +1,24 @@
+@echo off
+break off
+title Stream Finder Plugin Update Check
+PUSHD "%CD%"
+CD /D "%~dp0"
+goto main
+
+:main
+
+( echo $version = iwr -uri "https://raw.githubusercontent.com/DukeZilla/StreamFinderPlugin/main/data/StreamFinder/version.txt"
+  echo $version = $version.trim(^)
+  echo echo "$version" ^| Out-File -Encoding Ascii "version.txt"
+  echo exit ) > update-check.ps1 
+  
+powershell.exe -executionpolicy bypass -f "update-check.ps1"
+if %errorlevel%==1 (
+	color 4
+	echo An error has occurred when attempting to run:
+	echo "update-check.ps1"
+	echo.
+	timeout 5
+)
+del update-check.ps1
+exit
