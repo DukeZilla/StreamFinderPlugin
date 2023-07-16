e4193ca6b4659788ad763f9bbbf7e4d587e971b7
Rodriguez <59232365+DukeZilla@users.noreply.github.com>
2023-07-16 21:17:04 +0300

    Update version.h

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 670
+#define VERSION_BUILD 671
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
